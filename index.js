6a64693962358400326fd15d909c98ef8c4b30b6
Stephen O'Sullivan <stephenosullivan@Mac.tpg_nbn>
2026-04-02 19:32:43 +1100

    remove old index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const port = process.env.PORT
-const apiKey = process.env.API_KEY
-const dbUrl = process.env.DATABASE_URL
-
-console.log("Port:", port)
-console.log("API Key:", apiKey)
-console.log("DB:", dbUrl)
